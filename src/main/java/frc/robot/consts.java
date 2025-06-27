4c1441f5070415c90a16862a1606b4d4fea3765f
神楽坂贤司 <caoyifan2009sh@163.com>
2025-06-27 16:19:55 +0800

    KP

--- src/main/java/frc/robot/consts.java
+++ src/main/java/frc/robot/consts.java
@@ -2,7 +2,7 @@ package frc.robot;
 
 public class consts {
     public static final class PID {
-        public static final double KP = 10; // Proportional gain
+        public static final double KP = 5; // Proportional gain
         public static final double KI = 0.0; // Integral gain
         public static final double KD = 0.0; // Derivative gain
     }
