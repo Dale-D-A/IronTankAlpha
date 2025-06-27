7f8a830926013df77939f7cfb97e252387a83b5e
神楽坂贤司 <caoyifan2009sh@163.com>
2025-06-27 15:54:49 +0800

    modified:   src/main/java/frc/robot/consts.java

--- src/main/java/frc/robot/consts.java
+++ src/main/java/frc/robot/consts.java
@@ -2,10 +2,10 @@ package frc.robot;
 
 public class consts {
     public static final class PID {
-        public static final double KP = 0.1; // Proportional gain
+        public static final double KP = 10; // Proportional gain
         public static final double KI = 0.0; // Integral gain
         public static final double KD = 0.0; // Derivative gain
     }
 
-    public static final double testTurns = 1; // Number of turns to test
+    public static final double testTurns = 5; // Number of turns to test
 }
